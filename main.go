4ee8a75830c31178c55f29285959a02714fc7596
Hussain Ahmad <engrhussainahmad@gmail.com>
2022-01-06 04:55:43 +0500

    testValidity function implementation

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	fmt.Println("testing")
+	text := "23-ab-48-caba-56-haha"
+	fmt.Println(text)
+	testValidity(text)
+}
+
+func testValidity(text string) bool {
+	splitted := strings.Split(text, "-")
+	if len(splitted) >= 2 {
+		numberP := regexp.MustCompile(`^[0-9]+$`)
+		stringP := regexp.MustCompile(`^[a-zA-Z]+$`)
+
+		if numberP.MatchString(splitted[0]) && stringP.MatchString(splitted[1]) {
+			return true
+		}
+		log.Fatal("The format is not correct!")
+	} else {
+		log.Fatal("The format is not correct!")
+	}
+	return false
 }
