510ad3810a273f72b3dabd7be11afeb3e2e96b29
Hussain Ahmad <engrhussainahmad@gmail.com>
2022-01-06 07:40:14 +0500

    validity test case

---
+++ validate_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestTestValidity(t *testing.T) {
+	got := testValidity("12-ab-13-bc")
+	desired := true
+
+	if got != desired {
+		t.Error(fmt.Printf("got %v, want %v", got, desired))
+	}
+}
